2e83b9924433a6cc5caa9aab001fbd841bd9c102
nicola johnson <nicola.camp89@yahoo.com>
2024-09-19 13:20:47 +0100

    started portfolio page

--- js/load-content.js
+++ js/load-content.js
@@ -9,6 +9,21 @@ function loadContent(url, elementId) {
 
 
 document.addEventListener('DOMContentLoaded', function() {
+  // Load the navbar and footer
   loadContent('navbar.html', 'navbar');
   loadContent('footer.html', 'footer');
+
+  // JavaScript code to show more portfolio items
+  const viewMoreButton = document.getElementById('view-more');
+  const hiddenItems = document.querySelectorAll('.portfolio-item.hidden');
+
+  viewMoreButton.addEventListener('click', (event) => {
+    event.preventDefault(); // Prevent the page from scrolling to the top
+
+    hiddenItems.forEach(item => {
+      item.classList.remove('hidden'); // Remove 'hidden' class to show the items
+    });
+
+    viewMoreButton.style.display = 'none'; // Hide the "View More" button after clicking
+  });
 });
